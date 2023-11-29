37bd0a98080655f2dfaeda736be3453799814440
Mile <milenacapiglioni@gmail.com>
2023-11-29 18:45:43 +0100

    Initial commit

--- RFR_pipeline/func_delete.m
+++
@@ -1,7 +0,0 @@
-function myFunction(inputString)
-    if nargin == 0
-        disp('Function called without arguments.');
-    else
-        disp(['Input string received: ', inputString]);
-    end
-end
